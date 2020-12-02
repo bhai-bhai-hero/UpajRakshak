324977b5f244dd8ea168a1b4e2ff028359150d17
BISWAJIT MOHANTY <biswajittlh@gmail.com>
2020-12-02 11:42:12 +0530

    cookies

--- app.js
+++ app.js
@@ -3,27 +3,69 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var dishRouter = require('./routes/dishRouter');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 const mongoose= require('mongoose');
 //const Dishes = require('./models/dishes');
 const url = 'mongodb://localhost:27017/conFusion';
 var app = express();
+
+var dishRouter = require('./routes/dishRouter');
+var promoRouter = require('./routes/promoRouter');
+var leaderRouter = require('./routes/leaderRouter');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 const connect = mongoose.connect(url);
 
 connect.then((db)=>{
 	console.log("connected to server");
 },(err)=>{console.log(err);});
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use('/dishes',dishRouter);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
+function auth(req,res,next){
+	if(!req.signedCookies.user){
+		var authHeader=req.headers.authorization;
+	if(!authHeader){
+		var err= new Error('you are not authorized');
+		res.setHeader('WWW-Authenticate','Basic');
+        err.status = 401;
+        next(err);
+        return;
+	}var auth= new Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':');
+	var user=auth[0];
+	var pass=auth[1];
+	if(user=='admin'&&pass=='password'){
+		res.cookie('user','admin',{signed: true});
+		next();
+	} else {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');      
+      err.status = 401;
+      next(err);
+  }
+	} else {
+      if (req.signedCookies.user === 'admin') {
+          next();
+      }
+      else {
+          var err = new Error('You are not authenticated!');
+          err.status = 401;
+          next(err);
+      }
+  }
+	
+	
+}
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/dishes',dishRouter);
+app.use('/leaders',leaderRouter);
+app.use('/promotions',promoRouter)
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
